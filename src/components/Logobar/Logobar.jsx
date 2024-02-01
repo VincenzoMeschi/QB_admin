d39b3c9dc3ddd8fa7d5c7f6ce13097332b5d3a60
Vinny Windows <vincenzomeschi12@gmail.com>
2024-01-31 20:53:00 -0500

    Remove unused vars

--- src/components/Logobar/Logobar.jsx
+++ src/components/Logobar/Logobar.jsx
@@ -1,20 +1,19 @@
 import "./logobar.scss";
-import logo from "../../assets/Ruff_Logo-min.svg";
 
 const Logobar = () => {
-  return (
-    <div className="logobar">
-      <img
-        src={
-          "https://assets-global.website-files.com/60c27b463f005335cfba29e9/60c29a17b05f08e62e90d0c8_logo.svg"
-        }
-        width={273.09}
-        height={64}
-        alt=""
-      />
-      <h3>Throw It Deep Admin</h3>
-    </div>
-  );
+    return (
+        <div className="logobar">
+            <img
+                src={
+                    "https://assets-global.website-files.com/60c27b463f005335cfba29e9/60c29a17b05f08e62e90d0c8_logo.svg"
+                }
+                width={273.09}
+                height={64}
+                alt=""
+            />
+            <h3>Throw It Deep Admin</h3>
+        </div>
+    );
 };
 
 export default Logobar;
